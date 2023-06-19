ac7af33e3035c15f47863b1cc03689dfdcae1b85
gozeon <goze.qiu@gmail.com>
2023-06-19 09:58:20 +0800

    fix(windows): use package filepath instead of path

--- utils/html_template.go
+++ utils/html_template.go
@@ -3,13 +3,14 @@ package utils
 import (
 	"html/template"
 	"io/fs"
-	"path"
+	"path/filepath"
 	"strings"
 )
 
 func getAllHtml(path string) []string {
 	var result []string
 	err := Afs.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		Log.Debug(info)
 		if strings.Contains(path, ".html") {
 			result = append(result, path)
 		}
@@ -24,8 +25,11 @@ func getAllHtml(path string) []string {
 func GetTemplate(htmlPath string) (*template.Template, error) {
 	exists, err := Afs.Exists(htmlPath)
 	if err == nil && exists {
-		name := path.Base(htmlPath)
-		dir := path.Dir(htmlPath)
+		// fix path in windows
+		// @see: https://stackoverflow.com/a/70539540
+		name := filepath.Base(htmlPath)
+		dir := filepath.Dir(htmlPath)
+
 		return template.New(name).ParseFiles(getAllHtml(dir)...)
 	}
 
